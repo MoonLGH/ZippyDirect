c202b6be5eccea729d6e4198072b8e2714774dc5
MoonLGH <moonlmc1212@gmail.com>
2022-02-07 19:48:56 +0700

    added ?link endpoint

--- src/app/routes/api.ts
+++ src/app/routes/api.ts
@@ -2,7 +2,7 @@ import * as Express from "express";
 import {parse} from "../../util/zippy.js";
 const APIRouter = Express.Router();
 
-APIRouter.get("/get", function(req:Express.Request, res:Express.Response, next?:Express.NextFunction) {
+APIRouter.get("/get", function(req:Express.Request, res:Express.Response) {
   const {link} = req.query;
 
   parse((link as string)).then(res.status(200).send.bind(res)).catch((err) =>{
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,17 @@
 import * as Express from "express";
-
+import {parse} from "../../util/zippy.js";
 const indexRouter = Express.Router();
 
 indexRouter.get("/", function(req:Express.Request, res:Express.Response) {
-  res.render("index");
+  if (req.query.link) {
+    parse((req.query.link as string)).then((obj) =>{
+      res.redirect(obj.link);
+    }).catch(() =>{
+      res.redirect("/");
+    });
+  } else {
+    res.render("index");
+  }
 });
 
 export {indexRouter};
