8a29d137d7b7ef1dfe7fca2b6fd155aa03c8b835
MoonLGH <moonlmc1212@gmail.com>
2022-02-07 19:50:55 +0700

    fix some typo

--- src/app/public/javascripts/index.js
+++ src/app/public/javascripts/index.js
@@ -10,6 +10,6 @@ async function get() {
     document.querySelector("#linkhere").innerText = "Link does not valid, please try again";
     return;
   }
-  document.querySelector("#linkhere").innerHTML = `Note: click on open in new tab or save as file, if normal click didn't work <br> <a class="btn btn-primary" href="${data.link}" target="_blank" rel="noopener noreferrer nofollow">Or click here to direct download</a>`;
+  document.querySelector("#linkhere").innerHTML = `Note: click on open in new tab/save as file, if normal click didn't work <br> <a class="btn btn-primary" href="${data.link}" target="_blank" rel="noopener noreferrer nofollow">Or click here to direct download</a>`;
   document.querySelector("#result").value = data.link;
 }
